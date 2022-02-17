9f001bc515162a97a9530b3bb4ae47113d26cede
hesanzz <hesana.biju@diagnal.com>
2022-02-18 01:01:49 +0530

    [theoInt] - theo implemented with cdn

--- src/component/theoplayer/functions/init.js
+++ src/component/theoplayer/functions/init.js
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-destructuring */
-import * as THEOplayer from 'theoplayer';
+// import * as THEOplayer from 'theoplayer';
 import forEach from "lodash/forEach";
 import coreEvents from "../../diagnal-webplayer/src/core/modules/coreEvents";
 import { log, yell } from "../../diagnal-webplayer/src/utils/common";
@@ -15,8 +15,8 @@ import { dispatchCustomEvent, select } from "../../diagnal-webplayer/src/utils/d
 function init(self) {
   log("Initializing player");
   const container = select(self.options.selector);
-  const createNativePlayer = () => new THEOplayer.Player(container, { 
-    libraryLocation: "https://cdn.myth.theoplayer.com/0584187f-9d9b-470f-9f58-01782ee461cb",
+  const createNativePlayer = () => new window.THEOplayer.Player(container, { 
+    libraryLocation: "https://cdn.myth.theoplayer.com/7aff3fa6-f92e-45f9-a40e-1bce9911b073/",
     license: "sZP7IYe6T6fL0SeiTSIK0Oz_0LetFSx10lC-TSbt3ZziCSggCLPlIDh_Iub6FOPlUY3zWokgbgjNIOf9fl5L0SCr3uxeFS5i0Sh-3QPeCZzr3lgeFSfk3S0rIuRcCSAgImfVfK4_bQgZCYxNWoryIQXzImf90SCz0laZ3u0Z0l0r06i6Io4pIYP1UQgqWgjeCYxgflEc3l0r3LakTSfZ0LeoFOPeWok1dDrLYtA1Ioh6TgV6v6fVfKcqCoXVdQjLUOfVfGxEIDjiWQXrIYfpCoj-fgzVfKxqWDXNWG3ybojkbK3gflNWf6E6FOPVWo31WQ1qbta6FOPzdQ4qbQc1sD4ZFK3qWmPUFOPLIQ-LflNWfK1zWDikfgzVfG3gWKxydDkibK4LbogqW6f9UwPkIYz"
   });
   const source = {
@@ -26,7 +26,12 @@ function init(self) {
     }]
 }; 
   self.activePlayer = createNativePlayer();
-  self.activePlayer.source = source;
+  self.activePlayer.source = {
+    sources: {
+      src: 'https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8',
+      type: 'application/x-mpegurl'
+    }
+  };
 
   const plugins = self.options.plugins || [];
   forEach(plugins, plugin => plugin.onload(this));
